5b7ea478471f3966a52c327b79932476aa7a273b
FabricioFernandesSantos <fabricio.santos.wms@gmail.com>
2023-04-11 07:27:02 -0300

    Arquitetura MVC p

--- AppListaCurso/app/src/main/java/com/fstech/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/com/fstech/applistacurso/view/MainActivity.java
@@ -6,14 +6,23 @@ import android.os.Bundle;
 import android.widget.Button;
 
 import com.fstech.applistacurso.R;
+import com.fstech.applistacurso.model.Pessoa;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    Pessoa pessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pessoa = new Pessoa();
+        pessoa.setPrimeiroNome("Fabricio");
+        pessoa.setSobreNome("Santos");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("62-5555-6666");
 
     }
 }
