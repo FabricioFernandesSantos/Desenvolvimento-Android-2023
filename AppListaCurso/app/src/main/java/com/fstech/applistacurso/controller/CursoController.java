9bfc6dd663c0c0145cf0227cd898919a9c84577a
FabricioFernandesSantos <fabricio.santos.wms@gmail.com>
2023-04-28 07:06:45 -0300

    Refatorada ado layout Spinner 28/04

--- AppListaCurso/app/src/main/java/com/fstech/applistacurso/controller/CursoController.java
+++ AppListaCurso/app/src/main/java/com/fstech/applistacurso/controller/CursoController.java
@@ -1,4 +1,28 @@
 package com.fstech.applistacurso.controller;
 
+import com.fstech.applistacurso.model.Curso;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CursoController {
+
+    public List listCursos;
+
+    public List getListaDeCursos(){
+
+        listCursos = new ArrayList<Curso>();
+
+
+        listCursos.add(new Curso());
+        listCursos.add(new Curso());
+        listCursos.add(new Curso());
+        listCursos.add(new Curso());
+        listCursos.add(new Curso());
+        listCursos.add(new Curso());
+
+        return listCursos;
+
+
+    }
 }
--- AppListaCurso/app/src/main/java/com/fstech/applistacurso/model/Curso.java
+++ AppListaCurso/app/src/main/java/com/fstech/applistacurso/model/Curso.java
@@ -1,4 +1,14 @@
 package com.fstech.applistacurso.model;
 
 public class Curso {
+
+    private String nomeDoCursoDesejador;
+
+    public String getNomeDoCursoDesejador() {
+        return nomeDoCursoDesejador;
+    }
+
+    public void setNomeDoCursoDesejador(String nomeDoCursoDesejador) {
+        this.nomeDoCursoDesejador = nomeDoCursoDesejador;
+    }
 }
